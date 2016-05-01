de68a0228a4fdc980c8aed3c924b600ad7d9ca8b
takayush <ayush.tak@gmail.com>
2016-05-01 00:12:26 -0700

    Hascode, toString, and equal method generated with eclipse

--- src/customBot/graph/Node.java
+++ src/customBot/graph/Node.java
@@ -61,6 +61,74 @@ public class Node implements Comparable<Node>{
 	}
 
 
+	/* (non-Javadoc)
+	 * @see java.lang.Object#hashCode()
+	 */
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((destination == null) ? 0 : destination.hashCode());
+		result = prime * result + (hasRover ? 1231 : 1237);
+		result = prime * result + ((parent == null) ? 0 : parent.hashCode());
+		result = prime * result + ((science == null) ? 0 : science.hashCode());
+		result = prime * result + shortestDistance;
+		result = prime * result + ((terrain == null) ? 0 : terrain.hashCode());
+		result = prime * result + x;
+		result = prime * result + y;
+		return result;
+	}
+
+
+	/* (non-Javadoc)
+	 * @see java.lang.Object#equals(java.lang.Object)
+	 */
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Node other = (Node) obj;
+		if (destination == null) {
+			if (other.destination != null)
+				return false;
+		} else if (!destination.equals(other.destination))
+			return false;
+		if (hasRover != other.hasRover)
+			return false;
+		if (parent == null) {
+			if (other.parent != null)
+				return false;
+		} else if (!parent.equals(other.parent))
+			return false;
+		if (science != other.science)
+			return false;
+		if (shortestDistance != other.shortestDistance)
+			return false;
+		if (terrain != other.terrain)
+			return false;
+		if (x != other.x)
+			return false;
+		if (y != other.y)
+			return false;
+		return true;
+	}
+
+
+	/* (non-Javadoc)
+	 * @see java.lang.Object#toString()
+	 */
+	@Override
+	public String toString() {
+		return "Node [x=" + x + ", y=" + y + ", terrain=" + terrain + ", science=" + science + ", hasRover=" + hasRover
+				+ ", parent=" + parent + ", destination=" + destination + ", shortestDistance=" + shortestDistance
+				+ "]";
+	}
+
+
 	/**
 	 * @return the x
 	 */
