8ca7a8bc7b21b03cf7fedcaadf96d748d276f740
takayush <ayush.tak@gmail.com>
2016-04-30 23:53:52 -0700

    attributes of Nodes declared

--- src/customBot/graph/Node.java
+++ src/customBot/graph/Node.java
@@ -3,10 +3,35 @@
  */
 package customBot.graph;
 
+import enums.Science;
+import enums.Terrain;
+
 /**
  *Created by takayush on 4/30/2016
  *
  */
-public class Node {
+public class Node implements Comparable<Node>{
+	private int x;
+	private int y;
+	private Terrain terrain;
+	private Science science;
+	private boolean hasRover;
+	private Node parent;
+	private Node destination;
+	private int shortestDistance;
+	
+	//default constructor
+	public Node(){
+		
+	}
+
+	/* (non-Javadoc)
+	 * @see java.lang.Comparable#compareTo(java.lang.Object)
+	 */
+	@Override
+	public int compareTo(Node otherNode) {
+		// Fix me
+		return 0;
+	}
 
 }
